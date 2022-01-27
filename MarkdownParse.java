5e7b57d52cae84c169105c1dfb83caa1ab82a4b1
pei-lu <55153144+pei-lu@users.noreply.github.com>
2022-01-26 18:17:17 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,8 +15,11 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            if(markdown.substring(openParen + 1, closeParen).indexOf("!") == nextCloseBracket-1){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+            
         }
         return toReturn;
     }
